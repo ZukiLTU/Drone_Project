67fbf762c3a6bf90ef495e13237442cd997423ac
ZukiLTU <49497113+ZukiLTU@users.noreply.github.com>
2021-03-23 10:45:44 +0100

    Add files via upload

---
+++ Joystick/JoystickDetection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using DJI.WindowsSDK;
+using System.ServiceModel.Channels;
+using Windows.UI.Popups;
+using Windows.Media.Control;
+using SharpDX.DirectInput;
+
+namespace DJIDrone
+{
+    public class Joystick
+    {
+        void a()
+        {
+        
+        }
+        
+    }
+}
