30f32a8336a1d5b3fa74d2a157393021e4d802d3
ZukiLTU <49497113+ZukiLTU@users.noreply.github.com>
2021-03-25 19:47:12 +0100

    Add files via upload

---
+++ Composants/CHPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using DJI.WindowsSDK.Components;
+using Windows.UI.Popups;
+using DJIDrone.ViewModels;
+
+namespace DJIDrone.ComponentHandling
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class CHPage : Page
+    {
+        private FlightControllerHandler controllerHandler;
+        public CHPage()
+        {
+            this.InitializeComponent();
+            DataContext = new ComponentViewModel();
+        }
+
+        public async void btnDecollage_Click(object sender, RoutedEventArgs e)
+        {
+            try{
+                if(controllerHandler != null) 
+                {
+                    controllerHandler.StartTakeoffAsync();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageDialog message = new MessageDialog("Erreur : "+ ex.ToString());
+                //await message.ShowAsync();
+            }
+        }
+
+        private void btnRetour_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (controllerHandler != null)
+                {
+                    controllerHandler.StartAutoLandingAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
