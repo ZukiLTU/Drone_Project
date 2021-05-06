330c6fe861ff2a9b98e0da99e9dbac920128baa4
ZukiLTU <49497113+ZukiLTU@users.noreply.github.com>
2021-05-06 11:33:31 +0200

    Add files via upload

---
+++ Modèles/ViewModelBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace DJIDrone.ViewModels
+{
+    /// <summary>
+    /// Implémentation de base de <voir cref="INotifyPropertyChanged"/> interface.
+    /// </summary>
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Exécute lorsque cette propriété est modifiée.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Liée à l'event <voir cref="PropertyChanged"/>.
+        /// </summary>
+        protected void OnPropertyChanged([CallerMemberName] string changedPropertyName = "")
+        {
+            this.PropertyChangedOverride(changedPropertyName);
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(changedPropertyName));
+        }
+
+        /// <summary>
+        /// Point d'entrée.
+        /// </summary>
+        protected virtual void PropertyChangedOverride(string changedPropertyName)
+        {
+        }
+    }
+}
